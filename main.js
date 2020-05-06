15a7967ca1d11b0a80d916abd0216ef73625f822
gigtech-ret <55818261+gigtech-ret@users.noreply.github.com>
2020-05-06 09:29:26 +0800

    Bætti við helstu JavaScript skrá

---
+++ main.js
@@ -0,0 +1,35 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('NgSort')
+        .directive('sortable', function () {
+            return {
+                restrict: 'A',
+                transclude: true,
+                template:
+                    '<span ng-click="onClick_Sort();" class="sortable">' +
+                        '<span ng-transclude></span>' +
+                        '<i class="fa" ng-class="{\'fa-sort-desc\' : field === by && !reverse, \'fa-sort-asc\' : field === by && reverse}"></i>' +
+                    '</span>',
+                scope: {
+                    field: '=',
+                    by: '=',
+                    reverse: '='
+                },
+                link: function (scope, element, attrs) {
+                    scope.onClick_Sort = onClick_Sort;
+
+                    function onClick_Sort() {
+                        if (scope.field === scope.by) {
+                            scope.reverse = !scope.reverse;
+                        } else {
+                            scope.by = scope.field;
+                            scope.reverse = false;
+                        }
+                    }
+                }
+            };
+        });
+})(); 
+ 
